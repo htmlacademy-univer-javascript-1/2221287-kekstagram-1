3a68cf66ecfc3faa5b4371da290e85c8512a715f
Александр <belentsov02@mail.ru>
2022-10-02 22:36:32 +0500

    генерирует временные данные для дальнейшей разработки интерфейса

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 const COUNT = 25;
-const NAMES = ['Артём', 'Евгений', 'Александр'];
+
+const NAMES = [
+  'Артём',
+  'Евгений',
+  'Александр'
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const descriptions = [
+  'Фотокарточка',
+  'Получился случайный снимок!',
+  'Как вам эти фотографии?'
+];
 
 const Likes = {
   MIN: 15,
   MAX: 200
 };
+
+const arrayObjects = [];
 
 const getRandomInteger = (min,max) => {
   if (min < 0 || max < 0) {
@@ -17,24 +39,29 @@ const getRandomInteger = (min,max) => {
   return Math.floor(Math.random() * (max - min +1)) + min;
 };
 
-const addComments = () => ({
-  id: 135,
-  avatar: 'img/avatar-6.svg',
-  message: 'В целом всё неплохо. Но не всё.',
-  name: NAMES[getRandomInteger(0, NAMES.length - 1)]
-});
-
-const addPhoto = (id) => {
-  id++;
-  return {
-    id: id,
-    url: `img/avatar-${id + 1}.svg`,
-    description: 'В целом всё неплохо. Но не всё.',
-    likes: getRandomInteger(Likes.MIN, Likes.MAX),
-    comments: addComments()
-  };
+const arrayComments = (item) => {
+  const array = [];
+  for (let i = 0; i < item; i++){
+    array.push({
+      id: i,
+      avatar: 'img/avatar-{{getRandomInt(1, 6)}}.svg',
+      message: MESSAGE[getRandomInteger(0, MESSAGE.length - 1)],
+      name: NAMES[getRandomInteger(0, NAMES.length - 1)]
+    });
+  }
+  return array;
 };
 
-const photos = Array.from({length: COUNT}, addPhoto);
+const addPhotos = () => {
+  for (let i = 0; i < COUNT; i++){
+    arrayObjects.push({
+      id: i,
+      url: 'photos/{{i + 1}}.jpg',
+      description: descriptions[getRandomInteger(0, descriptions.length - 1)],
+      likes: getRandomInteger(Likes.MIN, Likes.MAX),
+      comments: arrayComments(getRandomInteger(0, 2))
+    });
+  }
+};
 
-export {photos};
+addPhotos();
