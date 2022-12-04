70c82e82e47c326a28d3c9403ceeec0ebcb0ce36
Keks <keks@htmlacademy.ru>
2022-12-04 14:49:41 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -1,5 +1,5 @@
 import { renderPhotos } from './pictures.js';
-import {uploadForm} from './formupload.js';
+import {uploadForm} from './formUpload.js';
 import {setData} from './fetch.js';
 import {showUnloadingErrorMessage} from './utils.js';
 
